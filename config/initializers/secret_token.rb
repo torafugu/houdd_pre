44f2d15f39c3406faa879e69c2a3873a6ddb79cc
Yoshihito Kaneda <yoshihito.kaneda@gmail.com>
2012-09-16 17:30:43 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HouddPre::Application.config.secret_token = 'd7dc5bbbc494230df7dfa0fa90a1e56e63660592ea10700266bae2ec651fd03bfce18abf76bcbd65e536fbd1a6d1e049acbd68528090dd7756588ab39b4c56a1'
